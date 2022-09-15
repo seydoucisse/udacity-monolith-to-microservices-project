c2394749c93976dd92f5dc06fdb754dfbb8b1199
Seydou CISSE <cisse.seydou1@yahoo.com>
2022-09-15 20:46:47 +0100

    fix: remove secret from set_env

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagram_user
-export POSTGRES_PASSWORD=udagram-pwd
-export POSTGRES_HOST=udagram-db.cteycvcpgwsd.us-east-2.rds.amazonaws.com
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::udagram-bucket-050742182233-dev
+export AWS_BUCKET=
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
